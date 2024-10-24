f38ff1b7c2d500e2bc270ba0b866cc7e8a574422
SuHyun <46856071+SuHyun-git@users.noreply.github.com>
2024-10-24 16:44:16 +0900

    [update] 값 반환시 헤더에도 추가

--- src/main/java/com/study/basecode/domain/auth/controller/AuthController.java
+++ src/main/java/com/study/basecode/domain/auth/controller/AuthController.java
@@ -6,6 +6,7 @@ import com.study.basecode.domain.auth.dto.SiginupRequestDto;
 import com.study.basecode.domain.auth.dto.SiginupResponseDto;
 import com.study.basecode.domain.auth.service.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,12 +18,13 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/auth/signup")
-    public SiginupResponseDto signup(@RequestBody SiginupRequestDto siginupRequestDto) {
-        return authService.signup(siginupRequestDto);
+    public ResponseEntity<SiginupResponseDto> signup(@RequestBody SiginupRequestDto siginupRequestDto) {
+        return ResponseEntity.ok(authService.signup(siginupRequestDto));
     }
 
     @PostMapping("/auth/signin")
-    public SigininResponseDto siginin(@RequestBody SigininRequestDto sigininRequestDto) {
-        return authService.signin(sigininRequestDto);
+    public ResponseEntity<SigininResponseDto> siginin(@RequestBody SigininRequestDto sigininRequestDto) {
+        String token = authService.signin(sigininRequestDto).getBearerToken();
+        return ResponseEntity.ok().header("Authorization",token).body(authService.signin(sigininRequestDto));
     }
 }
